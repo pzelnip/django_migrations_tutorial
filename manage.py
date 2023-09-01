9bb1b343743ac4ec2a7686c3cb0f07750771fcbf
Adam Parkin <adam.parkin@zapier.com>
2023-09-01 14:52:36 -0700

    change default port to 6100

--- manage.py
+++ manage.py
@@ -3,11 +3,19 @@
 import os
 import sys
 
+from django.conf import settings
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_migrations_tutorial.settings')
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "django_migrations_tutorial.settings"
+    )
     try:
+        from django.core.management.commands.runserver import Command as runserver
+
+        runserver.default_port = getattr(settings, "DEFAULT_PORT", "6100")
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -18,5 +26,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
