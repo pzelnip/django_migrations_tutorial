562fc0a7c65787d2ab9ab9b6beb4bc6522ed846c
Adam Parkin <adam.parkin@zapier.com>
2023-09-11 07:48:42 -0700

    Commit what I had at end of dry run recording

--- django_migrations_tutorial/models.py
+++ django_migrations_tutorial/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Zapp(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    zap_definition_language = models.JSONField(default=dict)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
